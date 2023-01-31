af67a76078257cfc97f7c9f698a390f7dbaa4e8d
juampidemaio <demaiojuanpablo05@gmail.com>
2023-01-31 11:50:46 -0300

    cambio de codigo

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,11 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+//De Maio Juan Pablo
 function mostrar()
 {
-	alert("ok");
+	let nombre;
+	nombre = prompt ("cual es tu nombre?");
+	alert(nombre);
 }
 
